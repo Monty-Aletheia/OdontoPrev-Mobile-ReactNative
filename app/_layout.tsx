f46f7c11637d197f12382a9a0d66aa51f20b9dd9
Pedro Lucca <pedroluccaprofissional@gmail.com>
2025-03-13 11:01:38 -0300

    feat: add React Nativewind

---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import '../global';
+import { Slot } from 'expo-router';
+
+const AppLayout = () => {
+  return (
+    <>
+        <Slot/>
+    </>
+  )
+}
+
+export default AppLayout
+
+const styles = StyleSheet.create({})
