8390b918dfc728a1039c49c36bf12142fbb4c065
Pedro Lucca <pedroluccaprofissional@gmail.com>
2025-03-16 07:55:56 -0300

    feat: add Profile Page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 
 
 const Home = () => {
   return (
-    <View className='w-[100%] h-[100%]'>
-      <Text>homeScreen</Text>
+    <View className='flex-1 mt-[20%] mr-5 ml-5'>
+      
+      <View className='flex-1  items-center'>
 
-      <View className="flex-1"></View>
+        <View className="h-auto w-auto ">
+          <Image source={require("../../assets/images/user_picture.png")} className="w-52 h-52 self-center" />
+        </View>
+
+
+        <View>
+          <Text className='mt-20 color-dark_blue font-bold text-xl'>Pedro Lucca Medeiros Miranda</Text>
+        </View>
+
+        <View className='mt-20 bg-white w-[85%] mb-32 rounded-md shadow-lg flex-1 flex-row justify-between pr-10 pl-10 pt-10'>
+            
+          <View>
+            <Text className='color-dark_blue font-black'>Especialidade:</Text>
+            <Text className='color-dark_blue'>Odontologia</Text>
+          </View>
+
+          <View>
+            <Text className='color-dark_blue font-black'>Numero de CRO:</Text>
+            <Text className='color-dark_blue '>380129</Text>
+          </View>
+
+        </View>
+
+      </View>
+
 
     </View>
   )
